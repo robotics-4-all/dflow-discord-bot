d91aa5e0c59b37a015dce2b9486738e98b0c3b9f
Nikolas Malamas <malamasn@hotmail.com>
2022-12-08 16:10:02 +0200

    Add paths of apis as env variables

--- commands.py
+++ commands.py
@@ -6,8 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 intents = discord.Intents.all()
+intents.message_content = True
 
 token = os.getenv('DFLOW_BOT_TOKEN')
+rasa_domain_url = os.getenv('RASA_DOMAIN_URL')
+rasa_train_model_path = os.getenv('RASA_TRAIN_MODEL_PATH')
+rasa_token = os.getenv('RASA_TOKEN')
+dflow_domain_url = os.getenv('DFLOW_DOMAIN_URL')
+dflow_validate_path = os.getenv('DFLOW_VALIDATE_PATH')
+dflow_generate_path = os.getenv('DFLOW_GENERATE_PATH')
+rasa_chat_path = os.getenv('RASA_CHAT_PATH')
+
 
 bot_commands = commands.Bot(command_prefix="!", intents=intents)
 
