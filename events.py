bf2b212c367c4ed8e754b5ee81b85e6c2fb93cff
Nikolas Malamas <malamasn@hotmail.com>
2022-12-08 16:10:46 +0200

    Add rasa conversation event

--- events.py
+++ events.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import os
 import random
 from dotenv import load_dotenv
+import typing
+import requests
 
 load_dotenv()
 intents = discord.Intents.all()
@@ -26,21 +28,40 @@ bot_dialogue = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot_dialogue))
 
-
 @bot_dialogue.event
 async def on_message(message):
-    print("message-->", message)
+    print("message -->", message)
+    print("message content -->", message.content)
     if message.author == bot_dialogue.user:
         return
 
     if message.content.startswith('hi'):
         await message.channel.send('Hello!')
 
+    if message.content == '':
+        await message.channel.send('Please say something...')
+        return
 
-@bot_dialogue.command("ping")
-async def ping(ctx):
-  await ctx.send('Pong!')
-
+    username = str(message.author)
+    data = '{"sender": "' + username + '", "message": "' +  str(message.content) + '"}'
+    data = data.encode('utf-8')
+    url = f"{rasa_domain_url}{rasa_chat_path}"
+    try:
+        response = requests.post(url, data = data)
+        if response.ok:
+            data = json.loads(response.text)
+            print(data)
+            for item in data:
+                msg = item['text']
+                print(f"Rasa response --> {msg}")
+                await message.channel.send(msg)
+        else:
+            raise Exception
+    except:
+        msg = "There was a problem connecting with Rasa server."
+        print(msg)
+        await message.channel.send(msg)
+    return
 
 if __name__ == "__main__":
     bot_dialogue.run(token)
