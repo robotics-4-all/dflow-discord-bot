8eb2798048d98e03d863d73998c1898310b5c5f6
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-12-21 20:42:26 +0200

    .

--- events.py
+++ events.py
@@ -12,45 +12,48 @@ load_dotenv()
 intents = discord.Intents.all()
 intents.message_content = True
 
-token = os.getenv('DFLOW_BOT_TOKEN')
-rasa_domain_url = os.getenv('RASA_DOMAIN_URL')
+TOKEN = os.getenv('DFLOW_BOT_TOKEN')
+RASA_DOMAIN_URL = os.getenv('RASA_DOMAIN_URL')
 rasa_train_model_path = os.getenv('RASA_TRAIN_MODEL_PATH')
-rasa_chat_path = os.getenv('RASA_CHAT_PATH')
-rasa_token = os.getenv('RASA_TOKEN')
+RASA_CHAT_PATH = os.getenv('RASA_CHAT_PATH')
+RASA_TOKEN = os.getenv('RASA_TOKEN')
 dflow_domain_url = os.getenv('DFLOW_DOMAIN_URL')
 dflow_validate_path = os.getenv('DFLOW_VALIDATE_PATH')
 dflow_generate_path = os.getenv('DFLOW_GENERATE_PATH')
 
+RASA_DIALOGUE_URL = f"{RASA_DOMAIN_URL}{RASA_CHAT_PATH}"
+
 bot_dialogue = commands.Bot(command_prefix="!", intents=intents)
+
+
+def call_rasa_dialogue(msg: str, username: str):
+    data = '{"sender": "' + username + '", "message": "' +  str(msg) + '"}'
+    data = data.encode('utf-8')
+    response = requests.post(RASA_DIALOGUE_URL, data=data, verify=False)
+    print(f'Rasa response: {response.json()}')
+    return response
 
 
 @bot_dialogue.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot_dialogue))
+    print('Logged in as {0.user}'.format(bot_dialogue))
 
 @bot_dialogue.event
 async def on_message(message):
-    print("message -->", message)
-    print("message content -->", message.content)
+    print(f"Rasa request: {message.content}")
     if message.author == bot_dialogue.user:
         return
 
     if message.content.startswith('!'):
         return
 
-    if message.content.startswith('hi'):
-        await message.channel.send('Hello!')
-
     if message.content == '':
         await message.channel.send('Please say something...')
         return
 
     username = str(message.author).replace("#",'').replace(".",'').replace(" ",'')
-    data = '{"sender": "' + username + '", "message": "' +  str(message.content) + '"}'
-    data = data.encode('utf-8')
-    url = f"{rasa_domain_url}{rasa_chat_path}"
+    response = call_rasa_dialogue(message.content, username)
     try:
-        response = requests.post(url, data = data, verify=False)
         if response.ok:
             data = response.json()
             for item in data:
@@ -69,4 +72,5 @@ async def on_message(message):
     return
 
 if __name__ == "__main__":
-    bot_dialogue.run(token)
+    # resp = call_rasa_dialogue('Hello', 'klpanagi2483')
+    bot_dialogue.run(TOKEN)
