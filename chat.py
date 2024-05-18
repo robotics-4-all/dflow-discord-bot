a560c1b21e81a39c79cf5024a966d57bad9abcb2
Konstantinos Panayiotou <klpanagi@gmail.com>
2024-05-18 22:13:25 +0000

    Forward rasa deploy event to discord channel

---
+++ chat.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import discord
+from discord.ext import commands
+import os
+import random
+from dotenv import load_dotenv
+import typing
+import requests
+
+load_dotenv()
+intents = discord.Intents.all()
+intents.message_content = True
+
+BOT_TOKEN = os.getenv('DFLOW_BOT_TOKEN')
+RASA_DOMAIN_URL = os.getenv('RASA_DOMAIN_URL')
+RASA_TRAIN_MODEL_PATH = os.getenv('RASA_TRAIN_MODEL_PATH')
+RASA_CHAT_PATH = os.getenv('RASA_CHAT_PATH')
+RASA_TOKEN = os.getenv('RASA_TOKEN')
+RASA_DIALOGUE_URL = f"{RASA_DOMAIN_URL}{RASA_CHAT_PATH}"
+
+
+CHANNEL_IDS_TO_LISTEN = [1239936124340932748]
+
+command_char = '!'
+
+bot_dialogue = commands.Bot(command_prefix="!", intents=intents)
+
+
+
+def call_rasa_dialogue(msg: str, username: str):
+    try:
+        data = '{"sender": "' + username + '", "message": "' +  str(msg) + '"}'
+        data = data.encode('utf-8')
+        response = requests.post(RASA_DIALOGUE_URL, data=data, verify=False)
+        print(f'Rasa response: {response.json()}')
+        return response
+    except Exception as e:
+        print(f'[X] Failed to call RASA Dialogue - {e}')
+        return {}
+
+
+@bot_dialogue.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(bot_dialogue))
+
+
+@bot_dialogue.event
+async def on_message(message):
+    if message.channel.id not in CHANNEL_IDS_TO_LISTEN:
+        return
+    if command_char in message.content:
+        return
+    print(f"Rasa request: {message.content}")
+    if message.author == bot_dialogue.user:
+        return
+
+    if message.content.startswith('!'):
+        return
+
+    if message.content == '':
+        await message.channel.send('Please say something...')
+        return
+
+    username = str(message.author).split("#")[0].replace(".",'').replace(" ",'')
+    response = call_rasa_dialogue(message.content, username)
+    try:
+        if response.ok:
+            data = response.json()
+            for item in data:
+                if 'text' in item:
+                    msg = item['text']
+                elif 'custom' in item:
+                    msg = item['custom']
+                print(f"Rasa response --> {msg}")
+                await message.channel.send(msg)
+        else:
+            raise Exception
+    except Exception as e:
+        msg = "There was a problem connecting with Rasa server."
+        print(f'{msg}: {str(e)}')
+        await message.channel.send(msg)
+    return
+
+
+if __name__ == "__main__":
+    # resp = call_rasa_dialogue('Hello', 'klpanagi2483')
+    bot_dialogue.run(BOT_TOKEN)
---
+++ deploy_events.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import asyncio
+import discord
+from discord.ext import commands
+import os
+import random
+from dotenv import load_dotenv
+import typing
+import requests
+
+from commlib.msg import MessageHeader, PubSubMessage
+from commlib.node import Node
+from commlib.transports.mqtt import ConnectionParameters
+
+load_dotenv()
+intents = discord.Intents.all()
+intents.message_content = True
+
+BOT_TOKEN = os.getenv('DFLOW_BOT_TOKEN')
+RASA_DOMAIN_URL = os.getenv('RASA_DOMAIN_URL')
+RASA_TRAIN_MODEL_PATH = os.getenv('RASA_TRAIN_MODEL_PATH')
+RASA_CHAT_PATH = os.getenv('RASA_CHAT_PATH')
+RASA_TOKEN = os.getenv('RASA_TOKEN')
+RASA_DIALOGUE_URL = f"{RASA_DOMAIN_URL}{RASA_CHAT_PATH}"
+
+NOTIFICATIONS_MQTT_HOST = os.getenv("NOTIFICATIONS_MQTT_HOST", "locsys.issel.ee.auth.gr")
+NOTIFICATIONS_MQTT_PORT = int(os.getenv("NOTIFICATIONS_MQTT_PORT", "1883"))
+NOTIFICATIONS_MQTT_USERNAME = os.getenv("NOTIFICATIONS_MQTT_USERNAME", "")
+NOTIFICATIONS_MQTT_PASSWORD = os.getenv("NOTIFICATIONS_MQTT_PASSWORD", "")
+NOTIFICATIONS_MQTT_SSL = os.getenv("NOTIFICATIONS_MQTT_SSL", False)
+NOTIFICATIONS_MQTT_TOPIC = os.getenv("NOTIFICATIONS_MQTT_TOPIC", "rasacloud.deployments.events")
+
+DISCORD_CHANNEL = os.getenv("DISCORD_CHANNEL", 1239936124340932748)
+
+command_char = '!'
+
+discord_client = discord.Client(intents=intents)
+
+
+class Notification(PubSubMessage):
+    header: MessageHeader = MessageHeader()
+    msg: str = ""
+
+
+class DeployEvents:
+    def __init__(self):
+        conn_params = ConnectionParameters(
+            host=NOTIFICATIONS_MQTT_HOST,
+            port=NOTIFICATIONS_MQTT_PORT,
+            username=NOTIFICATIONS_MQTT_USERNAME,
+            password=NOTIFICATIONS_MQTT_PASSWORD,
+            ssl=NOTIFICATIONS_MQTT_SSL,
+        )
+        node = Node(
+            node_name="rasa_cloud_deployment_listener_discord",
+            connection_params=conn_params,
+            debug=False,
+        )
+        self._node = node
+        self._sub = node.create_subscriber(msg_type=Notification,
+                                           topic=NOTIFICATIONS_MQTT_TOPIC,
+                                           on_message=self.on_event)
+        self._node.run()
+
+    def on_event(self, msg):
+        if not discord_client:
+            return
+        self.discord_send_sync(msg.msg)
+
+    def discord_send_sync(self, msg):
+        channel = discord_client.get_channel(DISCORD_CHANNEL)
+        # asyncio.get_event_loop().create_task(channel.send(msg))
+        asyncio.run_coroutine_threadsafe(channel.send(msg), discord_client.loop)
+
+    @discord_client.event
+    async def discord_on_ready():
+        print('Logged in as {0.user}'.format(discord_client))
+
+
+if __name__ == "__main__":
+    # resp = call_rasa_dialogue('Hello', 'klpanagi2483')
+    listener = DeployEvents()
+    discord_client.run(BOT_TOKEN)
